08d948a825e5b44864949ec9bce5e90e66e3b320
YaroslavMakoveev <y-makoveev@mail.ru>
2025-09-18 21:54:19 +0300

    ddd

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,18 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import { Button } from "react-bootstrap";
+import { Button, Navbar, Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
+  const navigate = useNavigate();
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Brand href="#home">Fake Store API</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Button variant="outline-light"> Cart </Button>
+          <Button variant="outline-light" onClick={() => navigate("/cart")}>
+            {" "}
+            Cart{" "}
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
